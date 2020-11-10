522e434b1610cf58f999bb9cf55f71c46c6bb585
Elena Kozlova <hostelsdirector@gmail.com>
2020-11-10 15:19:15 +0300

    Добавлен метод поиска города по части имени. Сортировка по количеству жителей в городе

--- social_network/src/main/java/com/example/social_network/service/CityService.java
+++ social_network/src/main/java/com/example/social_network/service/CityService.java
@@ -10,9 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Сервисный слой для работы с сущностью City
  */
